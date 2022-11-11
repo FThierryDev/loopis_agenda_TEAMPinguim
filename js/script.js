738f1442ddd1b957dc599bb543cfd66de09fc5c9
Luis Felippe Marques <luis.felippe@academico.ifpb.edu.br>
2022-11-10 21:33:28 -0300

    feature #04: implementing activity page script and data on page

--- js/script.js
+++ js/script.js
@@ -7,102 +7,171 @@
 
 //localStorage.removeItem('item') //remove o item do localstorage
 
-//localStorage.clear();
-
 ///////////////////////////////////////////////////////////////// INICIO ////////////////////
 
 
-const scheduleItems = [];
-const dateItems = [];
+let scheduleItems = [];
+let dateItems = [];
 
+document.addEventListener('load', loadPage())
 
+function loadPage(){
+    // Adiciona um bloco com a data atual, caso não exista valores na variavel de datas do localstorage
 
+        let dateItemsJSON = localStorage.getItem('dateItems');
+        let arrayDateItems = JSON.parse(dateItemsJSON);
+        if(!arrayDateItems || arrayDateItems[0]==null){
+            // Pegando a data atual
+            let date = new Date(); 
 
-document.addEventListener('load', ()=>{
-    let datesJSON = localStorage.getItem('dateItems');
-    let arrayDates = JSON.parse(datesJSON);
-
-    if(arrayDates!=null || arrayDates[0]!=null){
-        arrayDates.forEach(objDate => {
-            dateItems.push(objDate);
-            addDate(objDate.day, objDate.month, objDate.year, objDate.dayOfWeek);
-        });
-    }
-
-    let scheduleJSON = localStorage.getItem('scheduleItems');
-    let arraySchedule = JSON.parse(scheduleJSON);
-
-    if(arraySchedule!=null || arraySchedule[0]!=null){
-        arraySchedule.forEach(objSchedule => {
-            scheduleItems.push(objSchedule);
-        });
-    }
-})
-
-
-
-const buttonDate = document.querySelectorAll('.eventos');
-
-// Adiciona evento de click as datas
-buttonDate.forEach(button => {
-    button.addEventListener('click', ()=>{
-        let scheduleJSON = localStorage.getItem('scheduleItems');
-        let scheduleArray = JSON.parse(scheduleJSON);
-        let dateCurrentButton = button.querySelector('#diaMes').innerHTML+'/'+button.querySelector('#ano').innerHTML
-
-        if(!scheduleArray || scheduleArray[0]==null){
-                let boxTarefas = document.querySelector('.box-tarefas');
-                let element = document.querySelector('#alertNoActivities');
-                if(element){
-                    boxTarefas.removeChild(element);
-                }
-                let h1 = document.createElement('h1');
-                h1.appendChild(document.createTextNode('Ainda não há tarefas para essa data, adicione alguma clicando no botão abaixo!'));
-                h1.setAttribute('id', 'alertNoActivities');
-                boxTarefas.appendChild(h1);
+            let currentMonth = date.getMonth(); // mês atual
+            let currentDay = date.getDate(); // dia atual
+            let currentYear = date.getFullYear();  // ano atual
+            let dayOfWeek = date.getDay();  // dia da semana em indice, sendo 0 o domingo e continua..
+            let dayOfWeekText;
+            switch(dayOfWeek){
+                case 0: dayOfWeekText = 'Domingo';
+                break;
+                case 1: dayOfWeekText = 'Segunda';
+                break;
+                case 2: dayOfWeekText = 'Terça';       
+                break;
+                case 3: dayOfWeekText = 'Quarta';
+                break;
+                case 4: dayOfWeekText = 'Quinta';
+                break;
+                case 5: dayOfWeekText = 'Sexta';
+                break;
+                case 6: dayOfWeekText = 'Sabádo';
+                break;
+            }
             
-        // Adiciona atividade na lista referente à data
-        }else{
-            let boxTarefas = document.querySelector('.box-tarefas');
-            let blocoTarefas = document.querySelectorAll('.blocoTarefas');
-            //verificando se já existe algum bloco de tarefas, para então limpar-los da tela antes de adicionar os blocos referentes a data
-            if(blocoTarefas){
-                blocoTarefas.forEach(bloco => {
-                    boxTarefas.removeChild(bloco);
+            let objDateToday = {
+                day: currentDay,
+                month: currentMonth,
+                year: currentYear,
+                dayOfWeek: dayOfWeekText
+            }
+            dateItems.push(objDateToday);
+            localStorage.setItem('dateItems', JSON.stringify(dateItems));  
+            dateItems=[];     
+        }
+
+            let datesLocalJSON = localStorage.getItem('dateItems');
+            let arrayCurrentDateItems = JSON.parse(datesLocalJSON);
+
+            arrayCurrentDateItems.forEach(objDate => {
+                dateItems.push(objDate);
+                addDate(objDate.day, objDate.month, objDate.year, objDate.dayOfWeek);
+            });
+
+            let scheduleItemsJSON = localStorage.getItem('scheduleItems');
+            let arrayScheduleItems = JSON.parse(scheduleItemsJSON);
+            if(arrayScheduleItems!=null){
+                arrayScheduleItems.forEach(element => {
+                    scheduleItems.push(element);
                 });
             }
 
-            scheduleArray.forEach(activity => {
-                addActivityContainer(activity.name, activity.description, activity.date);
-            });
+        
+}
+
+
+
+
+// Evento para ABRIR o modal de adição de atividades
+let addActivityButton = document.querySelector('#add-activity-button');
+addActivityButton.addEventListener('click', ()=>{
+    // Abre o modal de adição
+        let addDateInput = document.querySelector('.addDateInput')
+        let dateActivity = document.querySelector('#dateActivity').innerHTML
+        console.log(dateActivity)
+        let chars = dateActivity.split('/')
+        let newDate = `${chars[2]}-${chars[1]}-${chars[0]}`;
+        addDateInput.setAttribute('value', newDate);
+
+        let modal = document.querySelector('#modal');
+        let modalFormAdd = document.querySelector('#modal-form-add');
+        openModal(modal, modalFormAdd);
     
-        }
+
+   
+})
+
+// Evento no botão que FECHA o modal de adição de atividades
+    let closeAddModal = document.querySelector('#close-addModal');
+    closeAddModal.addEventListener('click', ()=>{
+        let modal = document.querySelector('#modal');
+        let modalFormAdd = document.querySelector('#modal-form-add');
+        closeModal(modal, modalFormAdd);
     })
 
-});
+// Função que fecha o modal
+    function closeModal(divModal, formModal){
+        divModal.setAttribute('style', 'display: none');
+        formModal.setAttribute('style', 'display: none');
+    }
+// Função que abre o modal
+    function openModal(divModal, formModal){
+        divModal.setAttribute('style', 'display: flex');
+        formModal.setAttribute('style', 'display: flex ');
+    }
 
 
-// Adiciona as atividades no localstorage
+// Evento no botão que salva uma atividade em localstorage
+let confirmButton = document.querySelector('.confirmButton');
+confirmButton.addEventListener('click', ()=>{
+    let nameInput = document.querySelector('.addNameInput').value;
+    let descInput = document.querySelector('.addDescInput').value;
+    let dateInput = document.querySelector('.addDateInput').value;
+
+    addActivityLocalStorage(nameInput, descInput, dateInput);
+
+    let modal = document.querySelector('#modal');
+    let modalFormAdd = document.querySelector('#modal-form-add');
+    closeModal(modal, modalFormAdd);
+
+
+}) 
+
+
+
+// Adiciona uma atividade no localstorage
 function addActivityLocalStorage (name, description, date){
-    localStorage.removeItem('scheduleItems');
     let obj = {
-        name: name,
+        title: name,
         description: description,
         date: date
     }
 
     scheduleItems.push(obj);
+    localStorage.removeItem('scheduleItems');
     localStorage.setItem('scheduleItems', JSON.stringify(scheduleItems));
+
+    let dateButtons = document.querySelectorAll('.eventos');
+    dateButtons.forEach(dateButton => {
+        let dayMonth = dateButton.querySelector('#diaMes').innerHTML
+        let year = dateButton.querySelector('#ano').innerHTML
+        let chars = dayMonth.split('/');
+        let newDate = `${year}-${chars[1]}-${chars[0]}`
+        if(newDate == date){
+            
+            dateButton.dispatchEvent(new Event('click'));
+        }
+    });
+    
 }
-
-
 
 
 // Adiciona uma atividade no container
 function addActivityContainer(title, description, date){
     let boxTarefas = document.querySelector('.box-tarefas');
     let dateActivity = document.querySelector('#dateActivity');
-    dateActivity.innerHTML = date;
+
+    let chars = date.split('-')
+    let newDate = `${chars[2]}/${chars[1]}/${chars[0]}`;
+    
+    dateActivity.innerHTML = newDate;
     
     let divBlocoTarefas = document.createElement('div');
     divBlocoTarefas.setAttribute('class', 'blocoTarefas');
@@ -161,3 +230,66 @@ function addDate(Day, Month, Year, dayOfWeek){
 
 
 }
+
+
+// Evento de click das datas
+const buttonDate = document.querySelectorAll('.eventos');
+
+
+// função para cada data
+buttonDate.forEach(button => {
+    button.addEventListener('click', ()=>{
+        let scheduleJSON = localStorage.getItem('scheduleItems');
+        let scheduleArray = JSON.parse(scheduleJSON);
+        let dayMonth = button.querySelector('#diaMes').innerHTML
+        let year = button.querySelector('#ano').innerHTML
+        let chars = dayMonth.split('/');
+        let dateButton = `${chars[0]}/${chars[1]}/${year}`
+        let newDate = `${year}-${chars[1]}-${chars[0]}`
+
+        // mudando a data do titulo do container
+            let dateActivity = document.querySelector('#dateActivity') 
+
+            dateActivity.innerHTML = "";
+            dateActivity.appendChild(document.createTextNode(dateButton))
+
+        if(!scheduleArray || scheduleArray[0]==null){
+                let boxTarefas = document.querySelector('.box-tarefas');
+                let element = document.querySelector('#alertNoActivities');
+                if(element){
+                    boxTarefas.removeChild(element);
+                }
+                let h1 = document.createElement('h1');
+                h1.appendChild(document.createTextNode('Ainda não há tarefas para essa data, adicione alguma clicando no botão abaixo!'));
+                h1.setAttribute('id', 'alertNoActivities');
+                boxTarefas.appendChild(h1);
+            
+        // Adiciona atividade na lista referente à data
+        }else{
+            let boxTarefas = document.querySelector('.box-tarefas');
+            let blocoTarefas = document.querySelectorAll('.blocoTarefas');
+            //verificando se já existe algum bloco de tarefas, para então limpar-los da tela antes de adicionar os blocos referentes a data
+            if(blocoTarefas){
+                blocoTarefas.forEach(bloco => {
+                    boxTarefas.removeChild(bloco);
+                });
+            }
+            // Adiciona as atividades na tela que correspondem a data do botão que foi clicado
+            let count = 0;
+            scheduleArray.forEach(activity => {
+                if(newDate === activity.date){
+                    addActivityContainer(activity.title, activity.description, activity.date)
+                    count ++;
+                }
+            });
+
+        }
+    })
+
+});
+
+
+
+
+
+
